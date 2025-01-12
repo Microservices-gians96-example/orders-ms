cd7e791cb7f95c7e6090334590cbc1ef9303810e
Gianmarcos <gians96@gmail.com>
2025-01-11 21:03:19 -0500

    Nats config

--- README.md
+++ README.md
@@ -1,7 +1,22 @@
 # Orders Microservice
-
-## Run
-
 ```	
 docker-compose up -d
 ```
+## Development pasos
+
+1. Clonar el repositorio
+2. Instalar dependencias
+```	
+npm install
+```
+3. Crear el archivo .env basado en el archivo .env.template
+4. Levantar el servidor de Nats
+```
+docker run -d --name nats-main -p 4222:4222 -p 6222:6222 -p 8222:8222 nats
+```
+5. Iniciar el servidor de desarrollo
+```
+npm run start:dev
+```
+4. Iniciar el cliente
+
--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -5,7 +5,7 @@ import { PrismaClient } from '@prisma/client';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { OrderPaginationDto } from './dto/order-pagination.dto';
 import { ChangeOrderStatusDto } from './dto';
-import { PRODUCT_SERVICE } from 'src/config';
+import { NATS_SERVERS } from 'src/config';
 import { firstValueFrom } from 'rxjs';
 
 @Injectable()
@@ -13,7 +13,7 @@ export class OrdersService extends PrismaClient implements OnModuleInit {
   private readonly logger = new Logger('OrdersService');
 
 
-  constructor(@Inject(PRODUCT_SERVICE) private readonly productsClient: ClientProxy) {
+  constructor(@Inject(NATS_SERVERS) private readonly client: ClientProxy) {
     super();
   }
 
@@ -26,7 +26,7 @@ export class OrdersService extends PrismaClient implements OnModuleInit {
       //1 - Validar que los productos estén disponibles
       const productsIds = createOrderDto.items.map(item => item.productId)
 
-      const products = await firstValueFrom(this.productsClient.send({ cmd: 'validate_products' }, productsIds));
+      const products = await firstValueFrom(this.client.send({ cmd: 'validate_products' }, productsIds));
 
       //2 - Calcular los valores
       const totalAmount = createOrderDto.items.reduce((acc, orderItem) => {
@@ -125,7 +125,7 @@ export class OrdersService extends PrismaClient implements OnModuleInit {
       });
     }
     const productIds = order.OrderItem.map(item => item.productId)
-    const products = await firstValueFrom(this.productsClient.send({ cmd: 'validate_products' }, productIds));
+    const products = await firstValueFrom(this.client.send({ cmd: 'validate_products' }, productIds));
 
     return {
       ...order,
